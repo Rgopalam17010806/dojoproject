e083cf9cf5e953298753b425f4d81578a099beae
Ritu Gopalam <ritugopalam6@gmail.com>
2024-10-19 23:07:05 +0100

    have got the organise activity page to work

--- website/models.py
+++ website/models.py
@@ -1,5 +1,8 @@
+from wtforms import DateField, StringField, SubmitField, TimeField
 from website import db
 from flask_login import UserMixin
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired
 
 
 class User(db.Model, UserMixin):
@@ -10,3 +13,18 @@ class User(db.Model, UserMixin):
     last_name = db.Column(db.String(150))
     phone = db.Column(db.String(50))
     type = db.Column(db.String(10))
+
+
+class organiseactivity(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    activityname = db.Column(db.String(150), nullable=False)
+    activitydate = db.Column(db.Date, nullable=False)
+    activitytime = db.Column(db.Time, nullable=False)
+
+
+class organiseactivityForm(FlaskForm):
+    activityname = StringField('Name of the activity', validators=[DataRequired()])
+    activitydate = DateField('Date of Activity', format='%Y-%m-%d', validators=[DataRequired()])
+    activitytime = TimeField('Time of Activity', format='%H:%M', validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
--- website/views.py
+++ website/views.py
@@ -1,5 +1,9 @@
+from datetime import date
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
+from website import db
+
+from website.models import organiseactivity, organiseactivityForm
 
 views = Blueprint('views', __name__)
 
@@ -14,7 +18,18 @@ def home():
 def bookactivity():
     return render_template("bookactivity.html", user=current_user)
 
-@views.route('/organiseactivity')
+@views.route('/organiseactivity', methods=['GET','POST'])
 @login_required
-def organiseactivity():
-    return render_template("organiseactivity.html", user = current_user)
+def organiseactivity_view():
+    form = organiseactivityForm()
+    today_date = date.today().strftime('%Y-%m-%d')
+    if form.validate_on_submit():
+        new_activity = organiseactivity(
+            activityname=form.activityname.data,
+            activitydate=form.activitydate.data,
+            activitytime = form.activitytime.data
+        )
+        
+        db.session.add(new_activity)
+        db.session.commit()
+    return render_template("organiseactivity.html", user = current_user, form=form, today_date=today_date)
