ebdf5ab9a04ef7f72e37660773694c8abef611ae
Ritu Gopalam <ritugopalam6@gmail.com>
2024-10-19 23:56:38 +0100

    changed models.py

--- website/models.py
+++ website/models.py
@@ -5,26 +5,21 @@ from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired
 
 
-class User(db.Model, UserMixin):
+class User(db.Model, UserMixin):  
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(500))
-    first_name = db.Column(db.String(150))
-    last_name = db.Column(db.String(150))
-    phone = db.Column(db.String(50))
-    type = db.Column(db.String(10))
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    # Additional fields as needed
 
-
-class organiseactivity(db.Model):
+class organiseactivity(db.Model): 
     id = db.Column(db.Integer, primary_key=True)
     activityname = db.Column(db.String(150), nullable=False)
     activitydate = db.Column(db.Date, nullable=False)
     activitytime = db.Column(db.Time, nullable=False)
+    
 
-
-class organiseactivityForm(FlaskForm):
-    activityname = StringField('Name of the activity', validators=[DataRequired()])
-    activitydate = DateField('Date of Activity', format='%Y-%m-%d', validators=[DataRequired()])
-    activitytime = TimeField('Time of Activity', format='%H:%M', validators=[DataRequired()])
-    submit = SubmitField("Submit")
-
+class organiseactivityForm(FlaskForm):  # Example form using Flask-WTF
+    activityname = StringField('Activity Name', validators=[DataRequired()])
+    activitydate = DateField('Activity Date', validators=[DataRequired()])
+    activitytime = TimeField('Activity Time', validators=[DataRequired()])
+    submit = SubmitField('Organise Activity')
