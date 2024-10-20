484a5365561ef8fedd38c242ef7070d879275197
Ritu Gopalam <ritugopalam6@gmail.com>
2024-10-20 10:44:56 +0100

    modified models.py

--- website/models.py
+++ website/models.py
@@ -9,7 +9,10 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
-    # Additional fields as needed
+    first_name = db.Column(db.String(150))
+    last_name = db.Column(db.String(150))
+    phone = db.Column(db.String(50))
+    type = db.Column(db.String(10))
 
 class organiseactivity(db.Model): 
     id = db.Column(db.Integer, primary_key=True)
